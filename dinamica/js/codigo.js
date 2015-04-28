6c86cb68e7efd4c740ae1c39030a135889ab6c1d
felipeemerson <felipeemersonsb@hotmail.com>
2015-04-28 14:15:10 -0300

    Muda versão do bootstrap e adiciona glyphicons

--- dinamica/js/codigo.js
+++ dinamica/js/codigo.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
     $('#Adicionar1').click(function() {
-        $('#list1').append("<tr><td>"+ $("#Texto1").val() + "</td><td><button name='remover' class='btn btn-danger btn-lg'>Remover</button><button name='mover1' class='btn btn-primary btn-lg'>Mover</button></td></tr>");
+        $('#list1').append("<tr><td>"+ $("#Texto1").val() + "</td><td><button name='remover' class='btn btn-danger btn-sm'><span class='glyphicon glyphicon-remove'></span> Remover</button><button name='mover1' class='btn btn-primary btn-sm'><span class='glyphicon glyphicon-move'></span> Mover</button></td></tr>");
 $("#Texto1").val("");        
     });
 	
 	$('#Adicionar2').click(function() {
-        $('#list2').append("<tr><td>"+ $("#Texto2").val() +"</td><td><button name='remover' class='btn btn-danger btn-lg'>Remover</button><button name='mover2' class='btn btn-primary btn-lg'>Mover</button></td></tr>");
+        $('#list2').append("<tr><td>"+ $("#Texto2").val() +"</td><td><button name='remover' class='btn btn-danger btn-sm'><span class='glyphicon glyphicon-remove'></span> Remover</button><button name='mover2' class='btn btn-primary btn-sm'><span class='glyphicon glyphicon-move'></span> Mover</button></td></tr>");
 $("#Texto2").val("");        
     });
 
